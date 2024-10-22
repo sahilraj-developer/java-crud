4a0e42aa42c5f3fe8ce45e946ff0fbb7c5bc532c
sahil <sahil27raj@gmail.com>
2024-10-22 23:15:25 +0530

    add the project to create the crud operations with java

---
+++ src/main/java/com/example/CRUDapplication/CruDapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUDapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRUDapplication/CruDapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
